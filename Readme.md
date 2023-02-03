3e65de8aaf74b5b73c85a3e4f3c457650111ca31
Ankit Gupta <ankit.gupta@ripplr.in>
2023-02-03 22:41:25 +0530

    new style scroll to be completed

--- Readme.md
+++ Readme.md
@@ -5,3 +5,14 @@
 
 - So what's next? Well, this project is just getting started and is actively seeking more contributions.
     So if you have any ideas or improvements, we'd love to hear from you. Let's make this the best Matrix screensaver around!
+
+# Algothim for New Style Scroll
+
+- screen is a matrix which is devided into columns and each column has a random length.
+- each column either extends or falls.
+- columns keep extending from the base (x=n, y=0) of the screen until their legth has been reached.
+    write column extend algorithm.
+- once the columns have reached their length they start falling.
+    write down column fall algorithm
+
+
--- matrix.py
+++ matrix.py
@@ -4,10 +4,15 @@ import time
 import sys
 import signal
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='system.log')
 
 class LandEnd(Exception):
     pass
 
+class ColumnEnd(Exception):
+    pass
 
 class RiverEnd(Exception):
     pass
@@ -46,6 +51,28 @@ class River(object):
         return ' '
 
 
+class Column(object):
+
+    def __init__(self, t, x):
+        self.length = random.randint(1, t.height//1.5)
+        self.pos = 0
+        self.is_falling = False
+        self.t = t
+        self.languages = sanskrit + english + numbers
+        self.x = x
+
+    def extend(self, y, scene):
+        if self.pos < self.length:
+            self.pos += 1
+            scene[y][self.x] = random.choice(self.languages)
+        else:
+            scene[y][self.x] = random.choice(self.languages)
+            scene[y-self.length][self.x] = ' '
+            self.is_falling = True
+
+    def fall(self):
+        pass
+
 class SceneLine(object):
 
     def __init__(self, t):
@@ -72,7 +99,10 @@ class SceneLine(object):
         return ''.join([c for c in line])
 
 
-def matrix(t: Terminal, speed):
+def matrix_os(t: Terminal, speed):
+    """
+      Matrix old style scrolling
+    """
     sl = SceneLine(t)
     scene = ['' for y in range(t.height)]
 
@@ -85,12 +115,31 @@ def matrix(t: Terminal, speed):
                 scene = (lambda r: [sl.get_line(t)
                          for _ in range(r)] + scene[:-r])(speed)
 
+def matrix_ns(t: Terminal, speed):
+    """
+      Matrix new style scrolling
+    """
+    
+    scene = [[' ' for x in range(t.width)] for y in range(t.height)]
+    columns = [random.choice([Column(t, x), None]) if x%2 else None for x in range(t.width)]
+
+    with t.hidden_cursor():
+        while True:
+            for y in range(t.height):
+                for column in columns:
+                    if not column: continue
+                    column.extend(y, scene)
+
+                with t.location(0, 0):
+                    print("\n".join([''.join(line) for line in scene]), end='\r')
+                time.sleep(0.08)
 
 def main():
     try:
         t = Terminal()
         print(t.clear())
-        matrix(t, speed=2)
+#        matrix_os(t, speed=2)
+        matrix_ns(t, speed=2)
     except KeyboardInterrupt:
         print(t.clear())
         sys.exit(0)
