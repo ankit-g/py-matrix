acc54c9909f0a7c717c6e86db54e2b96987e8c63
ankit-g <ankitgupta.work@gmail.com>
2024-01-07 19:01:40 +0530

    Simplified Version

--- v2rain.py
+++ v2rain.py
@@ -1,17 +1,19 @@
-from pyneorain import languages
 from pyneorain import Bar
 from blessed import Terminal
 from collections import deque
-from time import sleep
+import asyncio
+import signal
+import sys
 
-def main():
-    t = Terminal()
+
+async def matrix_producer(t, aq):
     matrix = [[' ' for x in range(t.width)] for y in range(t.height)]
     columns = [deque([Bar(t, idx)]) if idx %2 else None 
                 for idx in range(t.width)]
     while True:
         for Q in columns:
-            if not Q: continue
+            if not Q:
+                continue
             new_bars = []
             for b in Q:
                 b.extend(matrix)
@@ -21,11 +23,58 @@ def main():
             Q.extend(new_bars)
             if Q[0].has_gone():
                 Q.popleft()
+        await aq.put(matrix)
+        await asyncio.sleep(1/24)
+
+
+async def matrix_consumer(t, aq):
+    def print_term(matrix):
         with t.hidden_cursor():
             with t.location(0, 0):
                 print('\n'.join([''.join(row) for row in matrix]), end='\r')
-        sleep(1/13)
+    while True:
+        await asyncio.to_thread(print_term, await aq.get())
+
+async def matrix_run():
+
+    try:
+        t = Terminal()
+        aq = asyncio.Queue(12)
+        await asyncio.gather(
+                    matrix_producer(t, aq),
+                    matrix_consumer(t, aq))
+    except asyncio.CancelledError:
+        raise
+
+sig_q = asyncio.Queue()
+
+async def handle_sig(sigtype):
+    await sig_q.put(sigtype)
+
+async def async_main():
+        t = Terminal()
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(signal.SIGWINCH, lambda:asyncio.create_task(handle_sig(signal.SIGWINCH)))
+        loop.add_signal_handler(signal.SIGINT, lambda:asyncio.create_task(handle_sig(signal.SIGINT)))
+        mtask = asyncio.create_task(matrix_run())
+        while True:
+            sig = await sig_q.get()
+            if sig == signal.SIGINT:
+                print(t.clear())
+                sys.exit(0)
+            elif sig == signal.SIGWINCH:
+                print(t.clear())
+                try:
+                    mtask.cancel()
+                    await mtask
+                except asyncio.CancelledError:
+                    pass
+                finally:
+                    mtask = asyncio.create_task(matrix_run())
+
+def main():
+    asyncio.run(async_main())
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
